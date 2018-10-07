f0ff327b475174603d0bb1d796dc9efa57d0fb6f
sayands <sayandsarkar.1997@gmail.com>
2018-10-07 22:46:11 +0530

    Added script for final predictions on test set and csv for submission

--- TGS-Salt-Identification/submission.py
+++ TGS-Salt-Identification/submission.py
@@ -324,3 +324,66 @@ plt.ylabel("IoU")
 plt.title("Threshold vs IoU ({}, {})".format(threshold_best, iou_best))
 plt.legend()
 
+# ===============================================================================================================
+# Sanity check using Adjusted IoU Threshold
+max_images = 60
+grid_width = 15
+grid_height = int(max_images / grid_width)
+fig, axs = plt.subplots(grid_height, grid_width, figsize=(grid_width, grid_height))
+for i, idx in enumerate(ids_valid[:max_images]):
+    img = train_df.loc[idx].images
+    mask = train_df.loc[idx].masks
+    pred = preds_valid[i]
+    ax = axs[int(i / grid_width), i % grid_width]
+    ax.imshow(img, cmap="Greys")
+    ax.imshow(mask, alpha=0.3, cmap="Greens")
+    ax.imshow(np.array(np.round(pred > threshold_best), dtype=np.float32), alpha=0.3, cmap="OrRd")
+    ax.text(1, img_size_ori-1, train_df.loc[idx].z, color="black")
+    ax.text(img_size_ori - 1, 1, round(train_df.loc[idx].coverage, 2), color="black", ha="right", va="top")
+    ax.text(1, 1, train_df.loc[idx].coverage_class, color="black", ha="left", va="top")
+    ax.set_yticklabels([])
+    ax.set_xticklabels([])
+plt.suptitle("Green: salt, Red: prediction. Top-left: coverage class, top-right: salt coverage, bottom-left: depth")
+
+# ==================================================================================================================
+# Predicting on test set and final submission
+def RLenc(img, order = 'F', format = True):
+      bytes = img.reshape(img.shape[0] * img.shape[1], order = order)
+  runs = [] # list of run lengths
+  r = 0 # current run length
+  pos = 1 # count starts from 1 per WK
+  for c in bytes:
+    if c == 0:
+      if r != 0:
+        runs.append((pos, r))
+        pos += r
+        r = 0
+      pos += 1
+    else:
+      r += 1
+  
+  # if last run is unsaved(i.e, data ends with 1)
+  if r!= 0:
+    runs.append((pos, r))
+    pos += r
+    r = 0
+  
+  if format:
+    z = ''
+    
+    for rr in runs:
+      z += '{} {}'.format(rr[0], rr[1])
+    return z[:-1]
+  else:
+    return runs
+
+x_test = np.array([upsample(np.array(load_img("/test/images/{}.png".format(idx), grayscale = True))) / 255 for idx in test_df.index]).reshape(-1, img_size_target, img_size_target, 1)
+
+preds_test = model.predict(x_test)
+
+pred_dict = {idx : RLenc(np.round(downsample(preds_test[i]) > threshold_best)) for i, idx in enumerate(test_df.index.values)}
+
+sub = pd.DataFrame.from_dict(pred_dict, orient = 'index')
+sub.index.names = ['id']
+sub.columns = ['rle_mask']
+sub.to_csv('submission.csv')
