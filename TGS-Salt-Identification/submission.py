dc9ff9489adec58b72458282a3fd4b83dc35e940
sayands <sayandsarkar.1997@gmail.com>
2018-10-07 19:13:51 +0530

    Added model defintion in keras

--- TGS-Salt-Identification/submission.py
+++ TGS-Salt-Identification/submission.py
@@ -97,3 +97,94 @@ for i, idx in enumerate(train_df.index[:max_images]):
 plt.suptitle("Green: salt. Top-left: coverage class, top-right: salt coverage, bottom-left: depth")
 
 # =======================================================================================================
+# Create Train/Test Split Stratified by Salt Coverage
+ids_train, ids_valid, x_train, x_valid, y_train, y_valid, cov_train, cov_test, depth_train, depth_test = train_test_split(
+    train_df.index.values,
+    np.array(train_df.images.map(upsample).tolist()).reshape(-1, img_size_target, img_size_target, 1), 
+    np.array(train_df.masks.map(upsample).tolist()).reshape(-1, img_size_target, img_size_target, 1), 
+    train_df.coverage.values,
+    train_df.z.values,
+    test_size=0.2, stratify=train_df.coverage_class, random_state=1337)
+
+print(x_train.shape)
+print(x_valid.shape)
+
+# Upscaling Images
+tmp_img = np.zeros((img_size_target, img_size_target), dtype = train_df.images.loc[ids_train[10]].dtype)
+tmp_img[:img_size_ori, :img_size_ori] = train_df.images.loc[ids_train[10]]
+fig, axs = plt.subplots(1, 2, figsize =(15, 5))
+axs[0].imshow(tmp_img, cmap ="Greys")
+axs[0].set_title("Original Image")
+axs[1].imshow(x_train[10].squeeze(), cmap = "Greys")
+axs[1].set_title("Scaled Image")
+# =========================================================================================================
+# Building Model
+def build_model(input_layer, start_neurons):
+  # 128->64
+  conv1 = Conv2D(start_neurons * 1, (3, 3), activation = 'relu', padding = 'same')(input_layer)
+  conv1 = Conv2D(start_neurons * 1, (3, 3), activation = 'relu',padding = 'same')(conv1)
+  pool1 = MaxPooling2D((2, 2))(conv1)
+  pool1 = Dropout(0.25)(pool1)
+  
+  # 64->32
+  conv2 = Conv2D(start_neurons * 2, (3, 3), activation = 'relu', padding = 'same')(pool1)
+  conv2 = Conv2D(start_neurons * 2, (3, 3), activation = 'relu', padding = 'same')(conv2)
+  pool2 = MaxPooling2D((2, 2))(conv2)
+  pool2 = Dropout(0.5)(pool2)
+  
+  # 32->16
+  conv3 = Conv2D(start_neurons * 4, (3, 3), activation = 'relu', padding = 'same')(pool2)
+  conv3 = Conv2D(start_neurons * 4, (3, 3), activation = 'relu', padding = 'same')(conv3)
+  pool3 = MaxPooling2D((2, 2))(conv3)
+  pool3 = Dropout(0.5)(pool3)
+  
+  # 16->8
+  conv4 = Conv2D(start_neurons * 8, (3, 3), activation = 'relu', padding = 'same')(pool3)
+  conv4 = Conv2D(start_neurons * 8, (3, 3), activation = 'relu', padding = 'same')(conv4)
+  pool4 = MaxPooling2D((2, 2))(conv4)
+  pool4 = Dropout(0.5)(pool4)
+  
+  # Middle
+  convm = Conv2D(start_neurons * 16, (3, 3), activation = 'relu', padding = 'same')(pool4)
+  convm = Conv2D(start_neurons * 16, (3, 3), activation = 'relu', padding = 'same')(convm)
+  
+  # 8->16
+  deconv4 = Conv2DTranspose(start_neurons * 8, (3, 3), strides = (2, 2), padding='same')(convm)
+  uconv4 = concatenate([deconv4, conv4])
+  uconv4 = Dropout(0.5)(uconv4)
+  uconv4 = Conv2D(start_neurons * 8, (3, 3), activation = 'relu', padding = 'same')(uconv4)
+  uconv4 = Conv2D(start_neurons * 8, (3, 3), activation = 'relu', padding = 'same')(uconv4)
+  
+  # 16->32
+  deconv3 = Conv2DTranspose(start_neurons * 4, (3, 3), strides = (2, 2), padding = 'same')(uconv4)
+  uconv3 = concatenate([deconv3, conv3])
+  uconv3 = Dropout(0.5)(uconv3)
+  uconv3 = Conv2D(start_neurons * 4,(3, 3), activation = 'relu', padding = 'same')(uconv3)
+  uconv3 = Conv2D(start_neurons * 4,(3, 3), activation ='relu', padding ='same')(uconv3)
+  
+  # 32->64
+  deconv2 = Conv2DTranspose(start_neurons * 2, (3, 3), strides = (2, 2), padding = 'same')(uconv3)
+  uconv2 = concatenate([deconv2, conv2])
+  uconv2 = Dropout(0.5)(uconv2)
+  uconv2 = Conv2D(start_neurons * 2, (3, 3), activation = 'relu', padding = 'same')(uconv2)
+  uconv2 = Conv2D(start_neurons * 2, (3, 3), activation = 'relu', padding = 'same')(uconv2)
+  
+  # 64->128
+  deconv1 = Conv2DTranspose(start_neurons * 1, (3, 3), strides = (2, 2), padding ='same')(uconv2)
+  uconv1 = concatenate([deconv1, conv1])
+  uconv1 = Dropout(0.5)(uconv1)
+  uconv1 = Conv2D(start_neurons * 1, (3, 3), activation = 'relu', padding = 'same')(uconv1)
+  uconv1 = Conv2D(start_neurons * 1, (3, 3), activation = 'relu', padding = 'same')(uconv1)
+  
+  output_layer = Conv2D(1, (1, 1), padding = 'same', activation = 'sigmoid')(uconv1)
+  
+  return output_layer
+
+input_layer = Input((img_size_target, img_size_target, 1))
+output_layer = build_model(input_layer, 16)
+
+model = Model(input_layer, output_layer)
+model.compile(loss = 'binary_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
+print(model.summary())
+
+# =========================================================================================================
